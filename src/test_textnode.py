81b033923b607f92ff6fbe3cec9b33e54a318848
hugermuger <toby@quasiso.de>
2025-11-19 12:38:27 +0100

    added HTMLNode

---
+++ src/test_textnode.py
@@ -0,0 +1,28 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_noteq(self):
+        node = TextNode("This is a text node", TextType.BOLD, "https://hugermuger.com")
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_noteq2(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.TEXT)
+        self.assertNotEqual(node, node2)
+
+    def test_url(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node.url, None)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -2,11 +2,12 @@ from enum import Enum
 
 
 class TextType(Enum):
-    BOLD_TEXT = "**Bold text**"
-    ITALIC_TEXT = "_Italic text_"
-    CODE_TEXT = "'Code text'"
-    LINK = "[anchor text](url)"
-    IMAGE = "![alt text](url)"
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
 
 
 class TextNode:
